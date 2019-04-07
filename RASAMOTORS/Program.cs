be9ec8d735c69cb37b51907b2fc3d6a0f2d1055b
onlinekee <onlinekee@gmail.com>
2019-04-07 14:48:49 +0530

    added employee

--- RASAMOTORS/Program.cs
+++ RASAMOTORS/Program.cs
@@ -17,6 +17,7 @@ namespace RASAMOTORS
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Home());
+
         }
     }
 }
